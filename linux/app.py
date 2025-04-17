2dd21c4389a9e99db3ae115ce5fd522e2137a85f
Alfie Terry <168709104+AlfieTerryyy@users.noreply.github.com>
2025-04-17 19:30:12 +0100

    Update app.py

--- linux/app.py
+++ linux/app.py
@@ -12,6 +12,7 @@ patterns = {
     r"tell me a joke": "Why did the chicken join a band? Because it had the drumsticks!",
     r"hello": "Hi there!",
     r"goodbye|bye": "Goodbye! See you soon!",
+    r"i am batman": "im a night stalking crime fighting vigilante and a heavy metal rapping machine",
 }
 
 #--------------------- Ensure DB exists
